a5de191f981b33df90b0f99cdf55b60dd64039c8
Mashrur Alam Khan <swastie@Mashrurs-MacBook-Pro.local>
2020-07-16 21:56:57 -0400

    Refactoring

--- APIs.js
+++ APIs.js
@@ -4,8 +4,7 @@ const port = 3000
 
 app.get('/hello', (req, res) => res.send('Hello World!'))
 app.get('/add', function(req, res) {
-    
     res.send("sum is  " + (parseInt(req.query.m1) + parseInt(req.query.m2)));
-  })
+})
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
