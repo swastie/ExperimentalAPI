b4bbcabc6f50b4e9b24db00e384ff5e31b9c8c7a
Walee <wmamun@cisco.com>
2020-07-16 21:55:12 -0400

    add

--- HelloWorld.js
+++ HelloWorld.js
@@ -3,5 +3,9 @@ const app = express()
 const port = 3000
 
 app.get('/hello', (req, res) => res.send('Hello World!'))
+app.get('/add', function(req, res) {
+    
+    res.send("sum is  " + (parseInt(req.query.m1) + parseInt(req.query.m2)));
+  })
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
